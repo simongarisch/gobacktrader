95b22c7826676a85e08f17e111cdb9d0297a8856
simongarisch <gatman946@gmail.com>
2021-03-08 10:41:04 +1100

    adding rounding functions to util

--- btutil/util.go
+++ btutil/util.go
@@ -1,8 +1,21 @@
 package btutil
 
-import "strings"
+import (
+	"math"
+	"strings"
+)
 
 // CleanString cleans and returns some input string.
 func CleanString(s string) string {
 	return strings.ToUpper(strings.TrimSpace(s))
+}
+
+// Round2dp rounds a number to two decimal places.
+func Round2dp(x float64) float64 {
+	return math.Round(x*100) / 100
+}
+
+// Round4dp rounds a number to four decimal places.
+func Round4dp(x float64) float64 {
+	return math.Round(x*10000) / 10000
 }
--- btutil/util_test.go
+++ btutil/util_test.go
@@ -9,3 +9,21 @@ func TestCleanString(t *testing.T) {
 		t.Fatalf("Bad formatting in CleanString: wanted '%s', got '%s'", expectedResult, result)
 	}
 }
+
+func TestRound2dp(t *testing.T) {
+	if Round2dp(1.222) != 1.22 {
+		t.Error("Rounding down error.")
+	}
+	if Round2dp(1.216) != 1.22 {
+		t.Error("Rounding up error.")
+	}
+}
+
+func TestRound4dp(t *testing.T) {
+	if Round4dp(1.22222) != 1.2222 {
+		t.Error("Rounding down error.")
+	}
+	if Round4dp(1.21116) != 1.2112 {
+		t.Error("Rounding up error.")
+	}
+}
