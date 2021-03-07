25691fb6ca6b3c39592d812e80a45c4a2c37b5b3
simongarisch <gatman946@gmail.com>
2021-03-07 14:49:50 +1100

    adding a cash asset

--- asset/cash.go
+++ asset/cash.go
@@ -1,15 +1,31 @@
 package asset
 
+import (
+	"fmt"
+	"gobacktrader/btutil"
+)
+
 var unitPrice = Price{Float64: 1.0, Valid: true}
 
 // Cash represents a cash asset.
 type Cash struct {
-	Asset
+	currency string
+}
+
+// GetCurrency returns the cash currency
+func (c Cash) GetCurrency() string {
+	return c.currency
 }
 
 // NewCash returns a new cash asset.
-func NewCash(currency string) error {
-	return nil
+func NewCash(currency string) (Cash, error) {
+	currency = btutil.CleanString(currency)
+	cash := Cash{currency: currency}
+	if len(currency) != 3 {
+		return cash, fmt.Errorf("'%s' is an invalid currency code", currency)
+	}
+
+	return cash, nil
 }
 
 // GetPrice returns the price for our cash asset.
--- asset/cash_test.go
+++ asset/cash_test.go
@@ -1,1 +1,47 @@
 package asset
+
+import "testing"
+
+func TestNewCash(t *testing.T) {
+	// start with a vaid currency
+	currency := "usd"
+	cash, err := NewCash(currency)
+	if err != nil {
+		t.Errorf("Error in NewCash - %s", err)
+	}
+	if cash.GetCurrency() != "USD" {
+		t.Errorf("Expected a '%s' currency code.", currency)
+	}
+
+	// and an invalid currency
+	currency = "usda"
+	_, err = NewCash(currency)
+	errstr := err.Error()
+	if errstr != "'USDA' is an invalid currency code" {
+		t.Error("Unexpected error string.")
+	}
+}
+
+func TestPriceValue(t *testing.T) {
+	cash, err := NewCash("USD")
+	if err != nil {
+		t.Errorf("Error in NewCash - %s", err)
+	}
+
+	price := cash.GetPrice()
+	value := cash.GetValue()
+
+	if !price.Valid {
+		t.Error("Price is invalid")
+	}
+	if !value.Valid {
+		t.Error("Value is invalid")
+	}
+
+	if price.Float64 != 1.0 {
+		t.Error("Price should be 1.0")
+	}
+	if value.Float64 != 1.0 {
+		t.Error("Valud should be 1.0")
+	}
+}
