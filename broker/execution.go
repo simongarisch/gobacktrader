d87295c4a078ca21ab9a6dbec5f889308b02fe88
simongarisch <gatman946@gmail.com>
2021-04-07 13:25:58 +1000

    adding broker execution strategies

--- broker/execution.go
+++ broker/execution.go
@@ -1,11 +1,51 @@
 package broker
 
-import "gobacktrader/trade"
+import (
+	"fmt"
+	"gobacktrader/btutil"
+	"gobacktrader/trade"
+)
 
 // FillAtLast executes a trade at the last available price.
 type FillAtLast struct{}
 
 // Execute executes a specific trade.
 func (e FillAtLast) Execute(trade trade.Trade) error {
+	consideration := trade.GetLocalCurrencyConsideration()
+	if !consideration.Valid {
+		tradeTicker := trade.GetAsset().GetTicker()
+		return fmt.Errorf("'%s' cannot execute a trade with invalid consideration", tradeTicker)
+	}
+
+	portfolio, asset, units := trade.GetPortfolio(), trade.GetAsset(), trade.GetUnits()
+	portfolio.Trade(asset, units, &consideration.Float64)
+	return nil
+}
+
+// FillAtLastWithSlippage executes a trade with some perecentage
+// slippage to the last available price.
+type FillAtLastWithSlippage struct {
+	slippage float64
+}
+
+// Execute executes a specific trade with slippage.
+func (e FillAtLastWithSlippage) Execute(trade trade.Trade) error {
+	consideration := trade.GetLocalCurrencyConsideration()
+	if !consideration.Valid {
+		tradeTicker := trade.GetAsset().GetTicker()
+		return fmt.Errorf("'%s' cannot execute a trade with invalid consideration", tradeTicker)
+	}
+
+	portfolio, asset, units := trade.GetPortfolio(), trade.GetAsset(), trade.GetUnits()
+	// slippage on buy trades will result in more consideration being paid
+	// slippage on sell trades will result in less consideration being received
+	considerationFloat := consideration.Float64
+	if btutil.Sgn(units) == +1.0 { // we are buyers and will have to pay more
+		considerationFloat = considerationFloat * (1 + e.slippage)
+	}
+	if btutil.Sgn(units) == -1.0 { // we are sellers and will receive less
+		considerationFloat = considerationFloat * (1 - e.slippage)
+	}
+	portfolio.Trade(asset, units, &considerationFloat)
 	return nil
 }
--- broker/execution_test.go
+++ broker/execution_test.go
@@ -1,2 +1,61 @@
 package broker
 
+import (
+	"gobacktrader/asset"
+	"gobacktrader/btutil"
+	"gobacktrader/trade"
+	"testing"
+)
+
+func TestFillAtLast(t *testing.T) {
+	portfolio, err1 := asset.NewPortfolio("XXX", "AUD")
+	stock, err2 := asset.NewStock("ZZB AU", "AUD")
+	cash, err3 := asset.NewCash("AUD")
+	if err := btutil.AnyValidError(err1, err2, err3); err != nil {
+		t.Errorf("Error in asset init - %s", err)
+	}
+
+	portfolio.Transfer(cash, 1000.0)
+	buyTrade := trade.NewTrade(portfolio, stock, +100.0)
+	sellTrade := trade.NewTrade(portfolio, stock, -100.0)
+
+	fillAtLast := FillAtLast{}
+	fillAtLastWithSlippage := FillAtLastWithSlippage{slippage: 0.02}
+
+	// we need a valid price to calculate trade consideration
+	err1 = fillAtLast.Execute(buyTrade)
+	err2 = fillAtLastWithSlippage.Execute(buyTrade)
+	for _, err := range []error{err1, err2} {
+		errStr := btutil.GetErrorString(err)
+		if errStr != "'ZZB AU' cannot execute a trade with invalid consideration" {
+			t.Errorf("Unexpected error string '%s'", errStr)
+		}
+	}
+
+	stock.SetPrice(asset.Price{Float64: 2.50, Valid: true})
+	// execute the buy trade
+	err := fillAtLast.Execute(buyTrade)
+	if err != nil {
+		t.Errorf("Error in FillAtLast{}.Execute() - %s", err)
+	}
+	if portfolio.GetUnits(stock) != 100 {
+		t.Error("Unexpected stock position")
+	}
+	if portfolio.GetUnits(cash) != 750 {
+		t.Error("Unexpected cash position")
+	}
+
+	// and the sell trade with some slippage
+	// trade is for $250 worth of stock
+	// 2% slippage = 0.02 * 250 = $5
+	err = fillAtLastWithSlippage.Execute(sellTrade)
+	if err != nil {
+		t.Errorf("Error in FillAtLastWithSlippage{}.Execute() - %s", err)
+	}
+	if portfolio.GetUnits(stock) != 0 { // position should be sold
+		t.Error("Unexpected stock position")
+	}
+	if portfolio.GetUnits(cash) != 995 { // the orig. 1K we started with less $5 slippage
+		t.Error("Unexpected cash position")
+	}
+}
