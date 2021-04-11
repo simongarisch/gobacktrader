2822cbae5e2de6e19323f25d2c27f15e9e469262
simongarisch <gatman946@gmail.com>
2021-04-11 14:53:30 +1000

    adding events.FetchNextGroup

--- events/events.go
+++ events/events.go
@@ -54,11 +54,44 @@ func (e *Events) Get() (IEvent, error) {
 	return event, nil
 }
 
+// FetchOne returns the most recent event.
+func (e *Events) FetchOne() (IEvent, error) {
+	return e.Get()
+}
+
 // FetchAll returns all events from the list.
 func (e *Events) FetchAll() []IEvent {
 	list := e.list
 	e.list = nil
 	return list
+}
+
+// FetchNextGroup returns all events that have the most recent time stamp.
+// For example, the next event time may be 11am for a stock price event,
+// but several FX rate events are also occurring at this time.
+func (e *Events) FetchNextGroup() ([]IEvent, error) {
+	var events []IEvent
+	nextEvent, err := e.FetchOne()
+	if err != nil {
+		return events, err
+	}
+
+	events = append(events, nextEvent)
+	nextEventTime := nextEvent.GetTime()
+	counter := 0
+	for _, event := range e.list {
+		if event.GetTime().Equal(nextEventTime) {
+			events = append(events, event)
+			counter++
+		} else {
+			break
+		}
+	}
+
+	if counter != 0 {
+		e.list = e.list[counter:]
+	}
+	return events, nil
 }
 
 // Contains returns true if some event is in the list, false otherwise
--- events/events_test.go
+++ events/events_test.go
@@ -27,6 +27,15 @@ func (t *testEvent) Process() {
 	t.isProcessed = true
 }
 
+func (t *testEvent) in(slice []IEvent) bool {
+	for _, e := range slice {
+		if t == e {
+			return true
+		}
+	}
+	return false
+}
+
 func TestEventsLen(t *testing.T) {
 	events := NewEvents()
 	if events.Len() != 0 {
@@ -117,5 +126,83 @@ func TestEventsAddGet(t *testing.T) {
 	}
 	if !eventsList[1].GetTime().Equal(time3) {
 		t.Error("Expecting event3 to be returned")
+	}
+}
+
+func TestEventsFetchNextGroup(t *testing.T) {
+	events := NewEvents()
+
+	time1 := time.Date(2020, time.December, 14, 0, 0, 0, 0, time.UTC)
+	time2 := time.Date(2020, time.December, 15, 0, 0, 0, 0, time.UTC)
+	time3 := time.Date(2020, time.December, 16, 0, 0, 0, 0, time.UTC)
+
+	t2e1 := newTestEvent(time2) // 2 events at time2
+	t2e2 := newTestEvent(time2)
+
+	t1e1 := newTestEvent(time1) // 3 events at time1
+	t1e2 := newTestEvent(time1)
+	t1e3 := newTestEvent(time1)
+
+	t3e1 := newTestEvent(time3) // 1 event at time3
+
+	// add all of these events to our events collection
+	allEvents := []IEvent{&t1e1, &t1e2, &t1e3, &t2e1, &t2e2, &t3e1}
+	for _, e := range allEvents {
+		events.Add(e)
+	}
+
+	// collect the first group
+	group1, err := events.FetchNextGroup()
+	if err != nil {
+		t.Errorf("Error in FetchNextGroup - %s", err)
+	}
+	if len(group1) != 3 {
+		t.Errorf("Expecting 3 events in group1, got %d", len(group1))
+	}
+	if !t1e1.in(group1) {
+		t.Error("t1e1 should be in group1")
+	}
+	if !t1e2.in(group1) {
+		t.Error("t1e2 should be in group1")
+	}
+	if !t1e3.in(group1) {
+		t.Error("t1e3 should be in group1")
+	}
+
+	// the second group
+	group2, err := events.FetchNextGroup()
+	if err != nil {
+		t.Errorf("Error in FetchNextGroup - %s", err)
+	}
+	if len(group2) != 2 {
+		t.Errorf("Expecting 2 events in group2, got %d", len(group2))
+	}
+	if !t2e1.in(group2) {
+		t.Error("t2e1 should be in group2")
+	}
+	if !t2e2.in(group2) {
+		t.Error("t2e2 should be in group2")
+	}
+
+	// third group
+	group3, err := events.FetchNextGroup()
+	if err != nil {
+		t.Errorf("Error in FetchNextGroup - %s", err)
+	}
+	if len(group3) != 1 {
+		t.Errorf("Expecting 1 event in group3, got %d", len(group3))
+	}
+	if !t3e1.in(group3) {
+		t.Error("t3e1 should be in group3")
+	}
+
+	// the events collection should now be empty
+	if !events.IsEmpty() {
+		t.Error("Expecting the events collection to be empty.")
+	}
+	_, err = events.FetchNextGroup()
+	errStr := btutil.GetErrorString(err)
+	if errStr != "the events list is empty" {
+		t.Errorf("Unexpected error string, got '%s'", errStr)
 	}
 }
