858d712b5cd9975f957aa5d6e92aa7b70d746fb3
simongarisch <gatman946@gmail.com>
2021-04-11 15:07:58 +1000

    adding docs

--- events/fx_rate_event.go
+++ events/fx_rate_event.go
@@ -1,14 +1,16 @@
 package events
 
 import (
-	"time"
 	"gobacktrader/asset"
+	"time"
 )
 
+// FxRateEvent attaches a time to an FX rate change.
 type FxRateEvent struct {
 	AssetPriceEvent
 }
 
+// NewFxRateEvent returns a new instance of FxRateEvent.
 func NewFxRateEvent(rate *asset.FxRate, eventTime time.Time, price asset.Price) AssetPriceEvent {
 	return NewAssetPriceEvent(rate, eventTime, price)
 }
